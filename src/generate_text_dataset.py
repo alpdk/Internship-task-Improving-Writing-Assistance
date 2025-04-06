464c1c7d69814d58aa55361f813777e7fad62a88
alpdk <alexander@podkopaev.net>
2025-04-06 13:07:35 +0300

    Changed max_length tokens

--- src/generate_text_dataset.py
+++ src/generate_text_dataset.py
@@ -60,7 +60,7 @@ def generate_text(model, prompt, num_samples, label):
 
         for _ in range(num_samples // len(languages)):
             model_chain = model(prompt,
-                                max_length=150,
+                                max_length=300,
                                 truncation=True,
                                 temperature=0.9,
                                 top_p=0.9,
