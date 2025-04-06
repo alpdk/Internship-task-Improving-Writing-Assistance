49743178a8d1b6a53be0a1ef716c3da53aa6bdd0
alpdk <alexander@podkopaev.net>
2025-04-06 13:34:36 +0300

    Changed prompt generation for better generation

--- src/generate_text_dataset.py
+++ src/generate_text_dataset.py
@@ -55,8 +55,8 @@ def generate_text(model, prompt, num_samples, label):
 
     for language in languages:
         prompt = (
-            f"Generate me please a text with no more than 100 words. This text should be written in {prompt} {language} style. "
-            f"Also, generate me an output, that will be containing only requested text, without additional symbols and sentences")
+            f"Generate me please a text with no more than 100 words. This text should be written in {language} language with usage of {prompt} style. "
+            f"Also, generate me an output, that will be containing only requested text, without additional information. I need ONLY text with on {language} language with {prompt} style.")
 
         for _ in range(num_samples // len(languages)):
             model_chain = model(prompt,
