fc7304c5754f42d753ff22015a94cc224dce69b3
alpdk <alexander@podkopaev.net>
2025-04-07 10:27:34 +0300

    Now summetric firstly try to check saved results

--- src/metrics/SumMetric.py
+++ src/metrics/SumMetric.py
@@ -1,3 +1,8 @@
+import os
+from pathlib import Path
+
+import pandas as pd
+
 from src.metrics.MetricsTemplate import MetricsTemplate
 from src.metrics.Accuracy import Accuracy
 from src.metrics.Recall import Recall
@@ -39,7 +44,18 @@ class SumMetric(MetricsTemplate):
         coef = [0.1, 0.2, 0.4, 0.3]
         res = 0
 
+        eval_path = Path(__file__).parent.parent / "metric_results" / f"eval_{self.dataset_name}"
+
+        df = pd.DataFrame()
+
+        if os.path.exists(eval_path):
+            df = pd.read_csv(eval_path, index_col=0)
+
         for i in range(len(metrics)):
-            res += metrics[i].evaluate_dataset() * coef[i]
+            if (self.metric_name in df.columns) and (self.form_approach.name in df.index) and (
+                    df.at[self.form_approach.name, self.metric_name] != 0):
+                res += df.at[self.form_approach.name, self.metric_name]
+            else:
+                res += metrics[i].evaluate_dataset() * coef[i]
 
         return res
